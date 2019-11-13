b8c73ee69bae24e114e2e0bb9933c5f75b78bffe
Guanghua Chi <guanghuachi@gmail.com>
2019-11-12 19:34:29 -0800

    change version; change intall requirement

--- setup.py
+++ setup.py
@@ -2,21 +2,18 @@ from distutils.core import setup
 setup(
   name = 'migration_detector',         # How you named your package folder (MyLib)
   packages = ['migration_detector'],   # Chose the same as "name"
-  version = '0.1.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A general approach to detect human migration events in digital trace data',   # Give a short description about your library
   author = 'Guanghua Chi',                   # Type in your name
   author_email = 'guanghua@berkeley.edu',      # Type in your E-Mail
   url = 'http://www.guanghuachi.com',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/g-chi/migration_detector/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/g-chi/migration_detector/archive/v0.1.2.tar.gz',    # I explain this later on
   keywords = ['migration', 'trajectory', 'trace data'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
           'numpy',
           'GraphLab-Create',
-          'os',
-          'copy',
-          'array',
           'matplotlib',
           'seaborn',
       ],
