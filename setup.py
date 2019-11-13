4be4ac90c5876d201e01ee6a03c29b162c0fe549
Guanghua Chi <guanghuachi@gmail.com>
2019-11-12 18:17:17 -0800

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'migration_detector',         # How you named your package folder (MyLib)
   packages = ['migration_detector'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A general approach to detect human migration events in digital trace data',   # Give a short description about your library
   author = 'Guanghua Chi',                   # Type in your name
